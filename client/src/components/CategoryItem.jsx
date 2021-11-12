ee6fc41770e1a2987d57bd3088dc5c42bb8464fe
Talita Beneves <talitabeneves15@gmail.com>
2021-11-12 05:05:53 -0300

    React Router

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,6 +1,20 @@
 import styled from 'styled-components';
 import  { mobile }  from '../responsive';
+import { Link } from "react-router-dom";
 
+const CategoryItem = ({item}) => {
+  return (
+    <Container>
+      <Link to={`/products/${item.cat}`}>
+        <Image src={item.img}/>
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>Shop Now</Button>
+        </Info>
+      </Link>
+    </Container>
+  )
+}
 
 const Container = styled.div`
   flex: 1;
@@ -8,12 +22,14 @@ const Container = styled.div`
   height: 70vh;
   position: relative;
 `;
+
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
   ${mobile({ height: "20vh" })}
 `;
+
 const Info = styled.div`
   position: absolute;
   top: 0;
@@ -25,10 +41,12 @@ const Info = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 const Title = styled.h1`
   color: white;
   margin-bottom: 20px;
 `;
+
 const Button = styled.button`
   border: none;
   padding: 10px;
@@ -39,16 +57,4 @@ const Button = styled.button`
   font-size: 18px
 `;  
 
-const CategoryItem = ({item}) => {
-  return (
-    <Container>
-      <Image src={item.img}/>
-      <Info>
-        <Title>{item.title}</Title>
-        <Button>Shop Now</Button>
-      </Info>
-    </Container>
-  )
-}
-
-export default CategoryItem
+export default CategoryItem;
