9bd073d06c7c82a903f5fe5d04cd1a9d1c365e86
Talita Beneves <talitabeneves15@gmail.com>
2021-11-14 02:58:53 -0300

    Admin Dashboard

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGRkYWNkZjE2YjRiMzFlZDBmMjFmNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNjc3NTc2MiwiZXhwIjoxNjM3MDM0OTYyfQ.fAFz9RUT8glce7XG31nXBKAFdoxs86lLg39l1GM2x3k";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
