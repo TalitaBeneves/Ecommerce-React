78fd39eb39dba29fd2abf6249cbf85e1b2b573c5
Talita Beneves <talitabeneves15@gmail.com>
2021-11-14 00:51:50 -0300

    Toolkit Persist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,17 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-import Success from "./pages/Success";
-
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
+import Success from "./pages/Success";
 import { useSelector } from "react-redux";
 
 const App = () => {
-  const user = useSelector(state => state.user.currentUser);
+  const user = useSelector((state) => state.user.currentUser);
   return (
     <Router>
       <Switch>
